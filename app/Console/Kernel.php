9d83eca1d0b3ed890898b83c5761fa659ec66115
abdelrahmanYouseff <abdelrahman.yousef@hadaf-hq.com>
2025-06-15 13:51:09 +0300

    Push full Laravel project with composer.json

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,21 +7,18 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+    protected $commands = [
+        \App\Console\Commands\CheckInactiveUsers::class,
+    ];
+
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('check:inactive-users')->everyMinute();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
