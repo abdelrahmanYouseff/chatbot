001abb04b28353cac08aa8656798a8439a49a040
abdelrahmanYouseff <abdelrahman.yousef@hadaf-hq.com>
2025-06-15 13:53:17 +0300

    Push full Laravel project with composer.json

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('check:inactive-users')->everyMinute();
-    }
+        $schedule->command('app:check-inactive-users')->everyMinute();
+        }
 
     protected function commands(): void
     {
